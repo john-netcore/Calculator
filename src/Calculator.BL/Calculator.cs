444db55f1a14693ace1ee60641a2a7b12434368b
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-07 19:04:48 +0200

    First commit.

---
+++ src/Calculator.BL/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator.BL
+{
+    public class Calculator
+    {
+        public int Calculate(int number1, int number2, string operation)
+        {
+            if (operation == "/")
+            {
+                return Divide(number1, number2);
+            }
+            else
+            {
+                System.Console.WriteLine("Unknown operation");
+                return 0;
+            }
+        }
+
+        private int Divide(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+    }
+}
